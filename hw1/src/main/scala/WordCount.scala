eab82feec7531c87532f8b138aa4363075e500d4
Nebular <wangxingyun0323@outlook.com>
2022-09-19 15:52:11 +0800

    complete the method not using spark

--- hw1/src/main/scala/WordCount.scala
+++ hw1/src/main/scala/WordCount.scala
@@ -1,18 +1,78 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+
+import java.io.{File, PrintWriter}
+import scala.collection.mutable
+import scala.io.Source
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-//    在电脑对应路径下应有一个用于存放单词的文件
-        val inputFile = "D:\\Code\\Course\\BigDataAnalysis\\hw1\\test.txt"
-        val conf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
+        val inputPath = new File("").getAbsoluteFile + "\\test.txt"
+        // 使用spark进行词频统计
+        useSpark(inputPath)
+        // 不使用spark进行词频统计
+        noUseSpark(inputPath)
+  }
 
-        val sc = new SparkContext(conf)
-        val textFile = sc.textFile(inputFile)
-        val wordCount = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).
-          reduceByKey((a, b) => a + b)
+  def useSpark(inputPath: String): Unit = {
+    // 创建spark配置
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
+    // 创建spark上下文环境
+    val sc = new SparkContext(conf)
+    // 通过文件系统构造RDD
+    val lines: RDD[String] = sc.textFile(inputPath)
+    // 将每一行按照空格切割合并，之后将每个单词小写
+    val lowerWords: RDD[String] = lines.flatMap(lines => lines.split(" ")).map(word => word.toLowerCase)
+    // 统计词频并且按照key值进行规约
+    val wordCount: RDD[(String,Int)] = lowerWords.map(word => (word,1)).reduceByKey((a, b) => a + b)
+    // 按照词频降序排序
+    val result = wordCount.sortBy(tuple => tuple._2, ascending = false)
+    // 将统计结果打印到控制台
+    result.collect().foreach(println)
+    // 关闭spark
+    sc.stop()
+  }
 
-        //将统计结果打印到控制台上
-        wordCount.collect().foreach(println)
+  def noUseSpark(inputPath: String): Unit = {
+    // 创建一个Map<Key:String Value:Int>，用于保存词频
+    val wordCount = mutable.Map.empty[String, Int]
+    // 获取文本
+    val lines = readData(inputPath)
+    // 将文本按照空格切割并且小写
+    val lowerWords = lines.flatMap(line => line.split(" ")).map(word => word.toLowerCase)
+    // 统计词频
+    lowerWords foreach{
+      word =>
+        if(wordCount.contains(word)) wordCount(word) += 1
+        else wordCount(word) = 1
+    }
+    // 按照词频降序排序
+    val result = wordCount.toList.sortBy(_._2).reverse
+    // 将统计结果打印在控制台上并保存结果
+    println(result.toArray.mkString("Array(", ", ", ")"))
+    saveResult(result)
+  }
+
+  def readData(inputPath: String): Iterator[String] = {
+    // 从文件读取数据
+    val data = Source.fromFile(inputPath, "UTF-8")
+    // 获取文本
+    val lines = data.getLines()
+    //返回结果
+    return lines
+  }
+
+  def saveResult(result: List[(String, Int)]): Unit = {
+    // 创建writer
+    val writer = new PrintWriter(new File("result.txt"))
+    result foreach {
+      tuple =>
+        val line = String.format("%-15s\t", tuple._1) + tuple._2 + "\n"
+        writer.write(line)
+    }
+    writer.close()
+
+
   }
 }
