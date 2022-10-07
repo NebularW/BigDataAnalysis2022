bb1bf91c7b72103047aed1ac443894218eaade60
Nebular <wangxingyun0323@outlook.com>
2022-10-07 17:13:11 +0800

    add dataset

---
+++ hw4/src/main/java/com/nju/Main.java
@@ -0,0 +1,12 @@
+package com.nju;
+
+import weka.core.Attribute;
+import weka.core.Instance;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.Instances;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
